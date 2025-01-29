5464573ea5ceb6c8b5c9800b7e9e9eea82b5f25f
unknown <njg.sty@gmail.com>
2025-01-29 09:08:30 +0800

    New Branch fish

---
+++ config.fish
@@ -0,0 +1,11 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+# Silent start ssh-agent
+#eval (ssh-agent -c)
+# Set up proxy
+#set -x HTTP_PROXY http://127.0.0.1:12334
+#set -x HTTPS_PROXY http://127.0.0.1:12334
+#set -x NO_PROXY "localhost,127.0.0.1,*.local"
+
+#start-ssh
